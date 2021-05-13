b0b458e99548ac4884e9b8ed58448a3ab4f5bf6c
Xvitu <vitor.amaral@creditas.com.br>
2021-05-13 15:32:58 -0300

    wip

---
+++ src/test/kotlin/com/creditas/codingdojo/boundaries/controllers/ChatControllerTest.kt
@@ -0,0 +1,17 @@
+package com.creditas.codingdojo.boundaries.controllers
+
+import com.creditas.codingdojo.domain.Chat
+import org.junit.jupiter.api.Test
+
+class ChatControllerTest {
+
+    @Test
+    fun `should start a chat`() {
+
+        val controller = ChatController()
+
+
+    }
+
+
+}
