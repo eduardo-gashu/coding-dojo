dcd37a2650b4ee8ccdbc18b0461f95a61135693e
eduardo-gashu <eduardo.gashu@gmail.com>
2021-05-17 16:25:18 -0300

    1
    Signed-off-by: eduardo-gashu <eduardo.gashu@gmail.com>

--- src/test/kotlin/com/creditas/codingdojo/boundaries/controllers/ChatControllerTest.kt
+++ src/test/kotlin/com/creditas/codingdojo/boundaries/controllers/ChatControllerTest.kt
@@ -1,30 +1,34 @@
 package com.creditas.codingdojo.boundaries.controllers
 
 import com.creditas.codingdojo.domain.Chat
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.amshove.kluent.`should be equal to`
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import javax.servlet.http.HttpServletResponse
 
 
-@SpringBootTest
-class ChatControllerTest {
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class ChatControllerTest(
     @Autowired
-    private lateinit var mockMvc: MockMvc
-
+    private val mockMvc: MockMvc
+){
     @Test
     fun `should return 201 for a started chat`() {
         val expectedChat = Chat()
+
+        val mapper = ObjectMapper()
 
         mockMvc.perform(
             MockMvcRequestBuilders.post("/chat")
         ).andExpect {
             it.response.status `should be equal to` HttpServletResponse.SC_CREATED
-            it.response.contentAsString `should be equal to` expectedChat.toString()
+            it.response.contentAsString `should be equal to` "{}"
         }
     }
 
